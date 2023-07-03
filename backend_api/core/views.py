491e09eaf2f401c9f7c59491417ef1c3d0658c33
NEJI <mrkochetov@yandex.ru>
2023-07-03 16:24:00 +0300

    react

--- backend_api/core/views.py
+++ backend_api/core/views.py
@@ -6,14 +6,23 @@ from rest_framework.response import Response
 # импорт статуса ответа из rest_framework
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 
-from . models import Todo
-
+from .models import Todo
+from .serializers import TodoSerializer
 
 
 def home(request):
     return render(request, 'home.html')
 
 
+@api_view(['GET', 'POST'])  # каккие запросы обрабатывает этот декоратор
 def todos_list(request):
-    todos = Todo.objects.all()
-    serializer = TodoSerializer()
+    if request.method =='GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        name = request.data.get('name')
+        todo = Todo.objects.create(name=name)
+        serializer = TodoSerializer(todo, many=False)
+        return Response(serializer.data)
+
---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { PlusIcon } from '@heroicons/react/24/solid'
+
+function App() {
+    const addTodoHandler = ()=>{
+        console.log('click')
+    }
+  return (
+ <div className="bg-slate-900 text-white h-screen relative">
+      <div className="flex flex-col w-full p-10">
+        <h1 className="text-5xl text-center pb-5">Todo App</h1>
+        <div className="flex items-center justify-between bg-slate-700 rounded-xl px-4">
+          <input
+            type="text"
+            className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
+            placeholder="Add ToDo here..."
+          />
+          <i onClick={addTodoHandler}>
+            <PlusIcon className="h-5 w-5 cursor-pointer icons hover:opacity-70" />
+          </i>
+
+          </div>
+      </div>
+ </div>
+  );
+}
+
+export default App;
